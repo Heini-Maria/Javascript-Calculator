4c470b6adb4b47d20b1cfd0199e3209c73f10ae0
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-12-07 12:06:10 +0100

    basic functions ready

--- script.js
+++ script.js
@@ -1,54 +1,103 @@
-
-const Calculator = {
-currentNum: '0',
-
-
-
-operate (num1, operator, num2) {
-if (operator === "+") {
-    return num1 + num2;
-} else if (operator === "-") {
-    return num1 - num2;
-} else if (operator === "&times;") {
-    return num1 * num2;
-} else if (operator === "÷") {
-    return num1 / num2;
-} else {
-    console.log("wooopsie!");
-}
-},
-
-appendNum (num) {
-    this.currentNum = num;    
-    /* console.log(Calculator.currentNum); */
-}
-
-}
-
 const screen = document.querySelector('.screen');
 const numbers = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.key');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const back = document.querySelector('.back');
+const decimal = document.querySelector('.decimal')
+
+
+const Calculator = {
+currentNum: '',
+operator: undefined,
+previousNum: '',
+
+
+operate () {
+    let result;
+    const prev = Number(this.previousNum);
+    const current = Number(this.currentNum);
+if (this.operator == "+") {
+    result = (prev + current);
+} else if (this.operator == "-") {
+    result = prev - current;
+} else if (this.operator == "*") {
+    result = prev * current;
+} else if (this.operator == "÷") {
+    result = prev / current;
+} else {
+    console.log("wooopsie!");
+}
+this.currentNum = result; 
+this.operator = undefined
+this.previousNum = ''
+console.log(result);
+screen.textContent = result
+
+},
+
+appendNum (num) {
+  this.currentNum = this.currentNum.toString() + num.toString();
+  console.log(this.currentNum)
+},
+chooseOperator (operator) {
+    if(!this.operator) {
+    this.operator = operator; 
+    console.log(this.currentNum);
+    this.previousNum = this.currentNum;
+    this.currentNum = '';
+    } else {
+        this.operator = operator;
+    }
+    console.log(this.operator);
+    
+    console.log(this.previousNum)
+    console.log(this.currentNum);
+},
+clear () {
+    this.currentNum = '';
+    this.previousNum = '';
+    this.operator = undefined;
+    screen.textContent = '';
+},
+back () {
+    this.currentNum = this.currentNum.toString().slice(0,-1);
+}
+
+}
 
 numbers.forEach((num) => {
    num.addEventListener ('click', () => {
-    if(Calculator.currentNum === '0') {
-        screen.textContent = num.innerText;
-        Calculator.appendNum(num.innerText);
-    } else {
-        const newNum = Calculator.currentNum + num.innerText;
-        screen.textContent = newNum;
-        Calculator.appendNum(newNum);
-    }
-    
-operators.forEach((operator) => {
-    operator.addEventListener ('click', () => {
-        screen.textContent = '';
-
-    })
-})   
+   Calculator.appendNum(num.innerText)
+   screen.textContent = Calculator.currentNum;
 });
 })
+operators.forEach((operator) => {
+    operator.addEventListener ('click', () => {
+        Calculator.chooseOperator(operator.innerText);
+        screen.textContent = Calculator.previousNum;
+    })
+})
+equal.addEventListener ('click', () => {
+    if(screen.textContent == '') {
+        screen.textContent = 0;
+    } else if(Calculator.previousNum == ''){
+        screen.textContent = Calculator.currentNum;    
+    } else {
+        Calculator.operate();
+    }
+})   
+clear.addEventListener ('click', () => {
+    Calculator.clear();
+    screen.textContent = 0;
+})
+back.addEventListener ('click', () => {
+    Calculator.back();
+    screen.textContent = Calculator.currentNum;
+})
+decimal.addEventListener ('click', () => {
+        if (!Calculator.currentNum.includes('.'))
+        Calculator.appendNum(decimal.innerText);
+        screen.textContent = Calculator.currentNum;
 
-   
-
-console.log(Calculator.currentNum); 
+})
